0d88211c556848a4d71f1c749a2022037cf075bb
Omar Daniel Esquivel Sanchez <omardanielesquivel@gmail.com>
2023-05-16 16:04:21 -0600

    update

--- static/assetsUsers/CreatedApp-50edeeaa.js
+++
@@ -1,2 +0,0 @@
-import{i as e,a as t,c as o,h as r}from"./index-64bef312.js";const s={},a={class:"page-container"},c=r("iframe",{style:{margin:"2rem auto","min-height":"100vh",width:"90%",overflow:"hidden"},src:"https://omararch48.github.io/sign_dist/",frameborder:"0"},`
-        `,-1),n=[c];function i(_,d){return t(),o("div",a,n)}const m=e(s,[["render",i]]);export{m as default};
